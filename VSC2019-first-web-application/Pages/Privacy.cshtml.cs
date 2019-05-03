8ab7a1f23877e4279d7e4342bcc7756c7f0e0e92
Tony Wang <zichutonywang@outlook.com>
2019-05-03 14:48:04 -0400

    Add project files.

---
+++ VSC2019-first-web-application/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VSC2019_first_web_application.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
