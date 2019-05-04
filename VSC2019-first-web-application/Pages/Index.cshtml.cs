a0ab8f751a5529aba478d290ccc6e7845b0f560d
Tony Wang <zichutonywang@outlook.com>
2019-05-04 02:59:44 -0400

    Added logic and message to homepage to display directory information.

--- VSC2019-first-web-application/Pages/Index.cshtml.cs
+++ VSC2019-first-web-application/Pages/Index.cshtml.cs
@@ -9,10 +9,15 @@ namespace VSC2019_first_web_application.Pages
 {
     public class IndexModel : PageModel
     {
-         public string Time { get; set; }
+        public string Time { get; set; }
+        public string Message { get; set; }
+
         public void OnGet()
         {
             Time = DateTime.Now.ToShortTimeString();
+
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
         }
     }
 }
